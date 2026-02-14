d426ba549f03c6a7f7c0586f5209be8a85127dc1
Maiser J Alves Oliva <cmaismaiser@gmail.com>
2026-02-14 20:30:23 -0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <h1 class="font-bold underline">{{ msg }}</h1>
+  <a href="https://tailwindcss.com/docs/installation/using-vite"
+    >https://tailwindcss.com/docs/installation/using-vite</a
+  >
+</template>
+
+<style scoped></style>
